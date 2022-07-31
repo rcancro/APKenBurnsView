a96226427c3041d85a8363b58ac6d5afc07ffcda
rcancro <rickycancro@gmail.com>
2022-07-31 16:15:47 -0700

    first try to make a swift package

---
+++ Tests/APKenBurnsViewTests/APKenBurnsViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import APKenBurnsView
+
+final class APKenBurnsViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(APKenBurnsView().text, "Hello, World!")
+    }
+}
